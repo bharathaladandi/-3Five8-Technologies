9e846d3c63807a45228cbabe3d77a39bd3fc6676
bharathaladandi <bharathaladandi@gmail.com>
2023-08-02 18:23:13 +0530

    Created Good UI and function working correctly

--- frontend/src/Components/Booking.jsx
+++ frontend/src/Components/Booking.jsx
@@ -1,17 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import {
+    Box,
+    Heading,
+    Select,
+    Input,
+    Button,
+    FormControl,
+    FormLabel,
+    FormHelperText,
+} from '@chakra-ui/react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Center, Square, Circle } from '@chakra-ui/react'
 
 const FacilitiesOption = [
     {
-        name:"Clubhouse",
-        slots:[
+        name: "Clubhouse",
+        slots: [
             { startTime: '10:00', endTime: '16:00', price: 100 },
             { startTime: '16:00', endTime: '22:00', price: 500 }
         ],
     },
     {
-        name:"Tennis Court",
-        slots:[
-            { startTime: '00:00', endTime: '23:59', price: 50 }
+        name: "Tennis Court",
+        slots: [
+            { startTime: '00:00', endTime: '24:00', price: 50 }
         ]
     }
 ];
@@ -19,50 +34,178 @@ const FacilitiesOption = [
 
 export const Booking = () => {
 
-    const [ facility, setFacility ] = useState('');
-    const [ startTime, setStartTime ] = useState('');
-    const [ endTime, setEndTime ] = useState("");
-    const [ date , setDate ] = useState("");
+    const [facility, setFacility] = useState('');
+    const [startTime, setStartTime] = useState('');
+    const [endTime, setEndTime] = useState("");
+    const [date, setDate] = useState("");
+    const [bookingStatus, setBookingStatus] = useState("");
+    const [bookingAmount, setBookingAmount] = useState("");
+
+    const handleBooking = () => {
+
+        if (!facility || !date || !startTime || !endTime) {
+            toast.error("Please Fill All Fields");
+            // setBookingAmount("0");
+            return;
+        }
+
+        // const selectDate = new Date(date).toDateString();
+
+        const isAlreadyBooked = false;
+
+        if (isAlreadyBooked) {
+            setBookingStatus("Already Booked");
+            // setBookingAmount("0");
+            return;
+        }
+
+        const selectSlotFacility = FacilitiesOption.find((e) => e.name === facility);
+
+        if (!selectSlotFacility) {
+
+            toast.error("Invalid Facility");
+            setBookingStatus("Invalid Facility")
+            // setBookingAmount("0");
+            return;
+        }
+
+        const selectSlot = selectSlotFacility.slots.find((e) =>
+            e.startTime <= startTime && e.endTime >= endTime);
+
+        if (!selectSlot) {
+            toast.error("Invalid Time Slot");
+            setBookingStatus("Invalid Time Slot")
+            // setBookingAmount("0");
+            return;
+        }
+
+        const hours = (new Date(`2023-01-01T${endTime}:00`).getTime() - new Date(`2023-01-01T${startTime}:00`).getTime()) / 3600000;
+
+        const bookingAmount = selectSlot.price * hours;
+
+        toast.success(`Booking Successful! Amount: Rs. ${bookingAmount}`, {
+            position: 'top-left',
+            autoClose: 3000,
+            hideProgressBar: true,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+        setDate(`${date}`)
+        setBookingAmount(`Rs.${bookingAmount}`)
+        setBookingStatus(`Booking Successful! Amount: Rs. ${bookingAmount}`)
+        // setBookingAmount({bookingAmount})
+
+        // Clear form fields after successful booking
+        setFacility('');
+        // setDate('');
+        setStartTime('');
+        setEndTime('');
+    }
 
 
-  return (
-    <div>
-        <h1>Book Your Facility</h1>
-        {/* Top part */}
-        <div>
-            <label> Select Facility : </label>
-            <select value={facility} onChange={(e) => setFacility(e.target.value)}>
-                <option value=""> Select Facility</option>
-                {FacilitiesOption.map((e) => (
-                    <option key={ e.name} value={e.name}> {e.name} </option>
-                ))}
-            </select>
-        </div>
 
-        {/* Middle Part  */}
-        <div>
-            <label> Date :</label>
-            <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
-        </div>
-        <div>
-            <label> Start Time :</label>
-            <input type="time" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
-        </div>
-        <div>
-            <label> End Time :</label>
-            <input type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
-        </div>
+    // return (
+    //     <div>
+    //         <h1>Book Your Facility</h1>
+    //         {/* Top part */}
+    //         <div>
+    //             <label> Facility : </label>
+    //             <select value={facility} onChange={(e) => setFacility(e.target.value)}>
+    //                 <option value=""> Select Facility</option>
+    //                 {FacilitiesOption.map((e) => (
+    //                     <option key={e.name} value={e.name}> {e.name} </option>
+    //                 ))}
+    //             </select>
+    //         </div>
 
-        <button>Book</button>
+    //         {/* Middle Part  */}
+    //         <div>
+    //             <label> Date :</label>
+    //             <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+    //         </div>
+    //         <div>
+    //             <label> Start Time :</label>
+    //             <input type="time" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
+    //         </div>
+    //         <div>
+    //             <label> End Time :</label>
+    //             <input type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
+    //         </div>
 
+    //         <button onClick={handleBooking}>Book</button>
+
+    //         <div>
+    //             <strong>Status : </strong>
+    //             {bookingStatus}
+    //         </div>
+    //         <div>
+    //             <strong>Booking Amount : </strong>
+    //             {bookingAmount}
+    //         </div>
+    //     </div>
+    // );
+
+    return (
         <div>
-            <strong>Status : </strong>
-            bookingStatus
+            <Box>
+                <Center>
+
+                    <Box p={4} w='450px' boxShadow={{ base: 'none', sm: 'md' }} >
+                        <ToastContainer />
+                        <Heading as="h2" mb={4}>
+                            Facility Booking
+                        </Heading>
+                        <FormControl mb={4}>
+                            <FormLabel>Facility:</FormLabel>
+                            <Select value={facility} onChange={(e) => setFacility(e.target.value)}>
+                                <option value="">Select Facility</option>
+                                {FacilitiesOption.map((f) => (
+                                    <option key={f.name} value={f.name}>
+                                        {f.name}
+                                    </option>
+                                ))}
+                            </Select>
+                        </FormControl>
+                        <FormControl mb={4}>
+                            <FormLabel>Date:</FormLabel>
+                            <Input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+                        </FormControl>
+                        <FormControl mb={4}>
+                            <FormLabel>Start Time:</FormLabel>
+                            <Input type="time" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
+                        </FormControl>
+                        <FormControl mb={4}>
+                            <FormLabel>End Time:</FormLabel>
+                            <Input type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
+                        </FormControl>
+                        <Button colorScheme="blue" onClick={handleBooking}>
+                            Book
+                        </Button>
+                      
+                    </Box>
+                </Center>
+                <Box>
+                </Box>
+                         {/* {bookingStatus && (
+                            <Box mt={4}>
+                                <strong>Status:</strong> {bookingStatus}
+                            </Box>
+                        )} */}
+                         {date && (
+                            <Box mt={4}>
+                                <strong>Booking Date:</strong> {date}
+                            </Box>
+                        )}
+                        {/* {bookingAmount && (
+                            <Box mt={4}>
+                                <strong>Booking Amount:</strong> {bookingAmount}
+                            </Box> */}
+                        {/* )} */}
+            
+                       
+            </Box>
         </div>
-        <div>
-            <strong>Booking Amount : </strong>
-            bookingAmount
-        </div>
-    </div>
-  );
+    )
 };
